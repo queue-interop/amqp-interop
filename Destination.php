05fa3867d0daf53cf281845bfe5382e4b3cf0a5c
Alexander Kozienko <pascalamsg@gmail.com>
2017-07-21 13:07:55 +0300

    init

---
+++ Destination.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Interop\Amqp;
+
+interface Destination
+{
+
+}
---
+++ Message.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Interop\Amqp;
+
+use Interop\Queue\PsrMessage;
+
+interface Message extends PsrMessage
+{
+    /**
+     * @param int $priority
+     */
+    public function setPriority($priority);
+
+    /**
+     * @return int
+     */
+    public function getPriority();
+
+    /**
+     * @param string $deliveryTag
+     */
+    public function setDeliveryTag($deliveryTag);
+
+    /**
+     * @return string
+     */
+    public function getDeliveryTag();
+}
