898447ad10c505dea368ede57af143212143b428
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2017-07-25 13:47:09 +0300

    add docblock to consumer

--- src/AmqpConsumer.php
+++ src/AmqpConsumer.php
@@ -4,6 +4,13 @@ namespace Interop\Amqp;
 
 use Interop\Queue\PsrConsumer;
 
+/**
+ * @method AmqpMessage|null receiveNoWait()
+ * @method AmqpMessage|null receive(int $timeout)
+ * @method AmqpQueue getQueue()
+ * @method void acknowledge(AmqpMessage $message)
+ * @method void reject(AmqpMessage $message, bool $requeue)
+ */
 interface AmqpConsumer extends PsrConsumer
 {
 }
