dd65b8ec0a235ff1ced456cfd4b1ea903b3cc512
Maksim Kotlyar <kotlyar.maksim@gmail.com>
2017-10-11 10:26:10 +0300

    Add basic consume methods.

--- src/AmqpContext.php
+++ src/AmqpContext.php
@@ -57,4 +57,34 @@ interface AmqpContext extends PsrContext
      * @param bool $global
      */
     public function setQos($prefetchSize, $prefetchCount, $global);
+
+    /**
+     * Notify broker that the channel is interested in consuming messages from this queue.
+     *
+     * A callback function to which the consumed message will be passed.
+     * The function must accept at a minimum one parameter, an \Interop\Amqp\AmqpMessage object,
+     * and an optional second parameter the \Interop\Amqp\AmqpConsumer from which the message was
+     * consumed. The \Interop\Amqp\AmqpContext::basicConsume() will not return the processing thread back to
+     * the PHP script until the callback function returns FALSE.
+     *
+     * @param AmqpConsumer $consumer
+     * @param callable     $callback
+     *
+     * @return void
+     */
+    public function subscribe(AmqpConsumer $consumer, callable $callback);
+
+    /**
+     * @param AmqpConsumer $consumer
+     *
+     * @return void
+     */
+    public function unsubscribe(AmqpConsumer $consumer);
+
+    /**
+     * @param float|int $timeout milliseconds, consumes endlessly if zero set
+     *
+     * @return void
+     */
+    public function consume($timeout = 0);
 }
